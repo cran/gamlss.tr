24f9edfa234b6a2f842c5529d436e33494489a5f
Mikis Stasinopoulos <d.stasinopoulos@londonmet.ac.uk>
2009-11-22 00:00:00 +0000

    version 3.1-0

--- R/trun.d.R
+++ R/trun.d.R
@@ -12,34 +12,34 @@ if (type!="both" && length(par)!= 1)  stop(paste("the length of par should be 1 
      pdf <- eval(parse(text=dfun))
      cdf <- eval(parse(text=pfun))
 fun <- if (type=="left")  
-       function(y, log = FALSE, ...)
+       function(x, log = FALSE, ...)
         {
-        if (distype=="Discrete" &&  any(y <= par))  
-          stop(paste("y must be greater than ", par, "\n", ""))
-        if (distype!="Discrete" && any(y < par))
-          stop(paste("y must be greater or equal than ", par, "\n", ""))
-        dfun <- pdf(y,log = TRUE,...)-log(1-cdf(par,...))
+        if (distype=="Discrete" &&  any(x <= par))  
+          stop(paste("x must be greater than ", par, "\n", ""))
+        if (distype!="Discrete" && any(x < par))
+          stop(paste("x must be greater or equal than ", par, "\n", ""))
+        dfun <- pdf(x,log = TRUE,...)-log(1-cdf(par,...))
         dfun <- if (log == TRUE) dfun else exp(dfun)
         dfun
        }
      else if (type=="right")
-      function(y, log = FALSE, ...)
+      function(x, log = FALSE, ...)
        {
-        if (any(y > par))  stop(paste("y must be less or equal to ", par, "\n", ""))
-        dfun <- pdf(y, log = TRUE,...)-log(cdf(par,...))
+        if (any(x > par))  stop(paste("x must be less or equal to ", par, "\n", ""))
+        dfun <- pdf(x, log = TRUE,...)-log(cdf(par,...))
         dfun <- if (log == TRUE) dfun else exp(dfun)
         dfun
        } 
      else if (type=="both")    
-      function(y, log = FALSE, ...)
+      function(x, log = FALSE, ...)
        {
-        if (distype=="Discrete" &&  (any(y <= par[1]) || any(y > par[2])) )  
-          stop(paste("y must be greater than", par[1], "and less than", par[2]+1, 
+        if (distype=="Discrete" &&  (any(x <= par[1]) || any(x > par[2])) )  
+          stop(paste("x must be greater than", par[1], "and less than", par[2]+1, 
                       "\n", ""))
-        if (distype!="Discrete" && (any(y < par[1]) || any(y > par[2])) )
-         stop(paste("y must be greater than", par[1], "and less or equal to", par[2], 
+        if (distype!="Discrete" && (any(x < par[1]) || any(x > par[2])) )
+         stop(paste("x must be greater than", par[1], "and less or equal to", par[2], 
                       "\n", ""))  
-        dfun <- pdf(y, log = TRUE,...) - log(cdf(par[2],...)-cdf(par[1],...))
+        dfun <- pdf(x, log = TRUE,...) - log(cdf(par[2],...)-cdf(par[1],...))
         dfun <- if (log == TRUE) dfun else exp(dfun)
         dfun
        }   
