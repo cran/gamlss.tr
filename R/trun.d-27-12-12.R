fbc599e4687fe396b4bb8b3bac2177ec659a2378
Mikis Stasinopoulos <d.stasinopoulos@londonmet.ac.uk>
2018-10-22 11:00:04 +0000

    version 5.1-0

--- R/trun.d-27-12-12.R
+++ R/trun.d-27-12-12.R
@@ -21,8 +21,8 @@ trun.d <-function(par, family = "NO", type = c("left", "right", "both"), varying
     {
       if (distype=="Discrete" &&  any(x <= par))  
         stop(paste("x must be greater than ", par, "\n", ""))
-      if (distype!="Discrete" && any(x < par))
-        stop(paste("x must be greater or equal than ", par, "\n", ""))
+       if (distype!="Discrete" && any(x < par))
+         stop(paste("x must be greater or equal than ", par, "\n", ""))
       dfun <- pdf(x,log = TRUE,...)-log(1-cdf(par,...))
       dfun <- if (log == TRUE) dfun else exp(dfun)
       dfun
@@ -31,9 +31,9 @@ trun.d <-function(par, family = "NO", type = c("left", "right", "both"), varying
     function(x, log = FALSE, ...)
     {
       if (distype=="Discrete" &&  any(x >= par))  
-        stop(paste("x must be less than ", par, "\n", ""))
+        stop(paste("x must be less than ", par,  "\n", ""))
       if (distype!="Discrete" && any(x > par))
-        stop(paste("x must be less or equal than ", par, "\n", ""))   
+        stop(paste("x must be less or equal than ", par,  "\n", ""))   
       dfun <-  if (distype=="Discrete") pdf(x, log = TRUE,...)-log(cdf(par-1,...))
                else pdf(x, log = TRUE,...)-log(cdf(par,...))
       dfun <- if (log == TRUE) dfun else exp(dfun)
@@ -42,21 +42,20 @@ trun.d <-function(par, family = "NO", type = c("left", "right", "both"), varying
   else if (type=="both")    
     function(x, log = FALSE, ...)
     {
-      if (distype=="Discrete" &&  (any(x <= par[1]) || any(x >= par[2])) )  
+      if (distype=="Discrete" && (any(x <= par[1]) || any(x >= par[2])) )  
         stop(paste("x must be greater than", par[1], "and less than", par[2], 
-                   "\n", ""))
+                   "for discrete distibutions", "\n", ""))
       if (distype!="Discrete" && (any(x < par[1]) || any(x > par[2])) )
         stop(paste("x must be greater than", par[1], "and less or equal to", par[2], 
-                   "\n", ""))  
+                   "for continuous distibutions", "\n", ""))  
       dfun <- if (distype=="Discrete") pdf(x, log = TRUE,...) - log(cdf(par[2]-1,...)-cdf(par[1],...)) 
               else pdf(x, log = TRUE,...) - log(cdf(par[2],...)-cdf(par[1],...))      
       dfun <- if (log == TRUE) dfun else exp(dfun)
       dfun
     }   
-  }
+  } # stop no varying papameters
   else # this is for varying truncation only 
   {
-  #  browser()
     if (type=="both" && dim(par)[2]!= 2)  stop(paste("the rows of par should be 2 \n"))
     #--
     fun <- if (type=="left")  
--- R/trun.r-28-12-12.R
+++ R/trun.r-28-12-12.R
@@ -22,6 +22,7 @@ fun <- if (type=="left")
      p <- runif(n)
     pp <- cdf(par,...)
      r <- invcdf((pp+p*(1-pp)),...)
+    if (distype=="Discrete")  r <- ifelse(r==par, par+1L, r)
      r
     }
      else if (type=="right")
@@ -32,6 +33,7 @@ fun <- if (type=="left")
      p <- runif(n)
     pp <- cdf(par,...)
      r <- invcdf(p*pp,...)
+     if (distype=="Discrete" )  r <- ifelse(r==par, par-1L, r)
      r
     }
      else if (type=="both")    
@@ -43,6 +45,8 @@ fun <- if (type=="left")
    pp1 <- cdf(par[1],...)
    pp2 <- cdf(par[2],...)
      r <- invcdf(p*(pp2-pp1)+pp1,...)
+     if (distype=="Discrete" ) { r <- ifelse(r==par[1], par[1]+1L, r)
+                                r <- ifelse(r==par[2], par[2]-1L, r)}
      r
     }
   }
